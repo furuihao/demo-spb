78803b7453496603b1c540a6be40ee03c6524c53
furuihao <94402146@qq.com>
2018-01-14 23:57:36 +0800

    create project

---
+++ src/main/java/com/hao/demospb/DemoSpbApplication.java
@@ -0,0 +1,15 @@
+package com.hao.demospb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class DemoSpbApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoSpbApplication.class, args);
+	}
+}
